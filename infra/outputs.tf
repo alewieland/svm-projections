6339c11f58b7052a9628652594f974919a833c12
Alexander Wieland <69452789+alewieland@users.noreply.github.com>
2025-06-17 16:27:10 +0200

    Add Firebase and BigQuery dev setup

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "cloud_run_url" {
   description = "URL of the deployed Cloud Run service"
   value       = google_cloud_run_service.backend.status[0].url
 }
+
+output "bigquery_dataset" {
+  description = "BigQuery dataset used for bet logging"
+  value       = google_bigquery_dataset.bets.dataset_id
+}
